198dba91a6c185c5637d1d425f940661189bfbfa
Kumpati Vishnu Vardhan <150310460+VishnuKumpati@users.noreply.github.com>
2025-01-16 17:23:25 +0530

    Update coinStatsService.js

--- app/service/coinStatsService.js
+++ app/service/coinStatsService.js
@@ -3,7 +3,7 @@ app.service('coinStatsService', function($http) {
         const options = {
             headers: {
                 'accept': 'application/json',
-                'X-API-KEY': '30nE9ahS0gb+DQO1SMROT6DNt30G44/eceiasWiYr/g='
+                'X-API-KEY': 'add key here bro...'
             }
         };
 
